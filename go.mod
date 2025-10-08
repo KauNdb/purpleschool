f5125f36bf939eb0fa828861079b92407eef3524
Mikhail <icemi.mikhail@mail.ru>
2025-10-08 18:46:56 +0200

    new commit2

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.24.0
 
 require github.com/joho/godotenv v1.5.1
 
-require github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
+require (
+	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
+	golang.org/x/crypto v0.42.0
+)
--- internal/verify/handler.go
+++ internal/verify/handler.go
@@ -4,17 +4,28 @@ import (
 	"encoding/json"
 	"fmt"
 	"http/configs"
+	"log"
 	"net/http"
 	"net/smtp"
+	"os"
 
 	"github.com/jordan-wright/email"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type EmailIns struct {
 	*configs.Config
 }
 
-const hashStr = "123"
+type User struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type JsonUser struct {
+	Email string `json:"email"`
+	Hash  string `json:"hash"`
+}
 
 func NewEmail(router *http.ServeMux, config *configs.Config) {
 	emailIns := &EmailIns{
@@ -26,15 +37,46 @@ func NewEmail(router *http.ServeMux, config *configs.Config) {
 
 func (handler *EmailIns) Send() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
+		var user User
+		err := json.NewDecoder(req.Body).Decode(&user)
+		if err != nil {
+			http.Error(w, "invalid JSON", http.StatusBadRequest)
+			return
+		}
+		defer req.Body.Close()
+
 		e := email.NewEmail()
-		FromMsg := fmt.Sprintf("Jordan Wright <%s>", handler.Email)
+		FromMsg := fmt.Sprintf("Jordan Wright <%s>", user.Email)
 		e.From = FromMsg
 		e.To = []string{handler.Addr}
 		e.Subject = "Awesome Subject"
-		text := fmt.Sprintf("http://localhost:8081/verify/%s", hashStr)
+		hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		jsonUser := JsonUser{
+			Email: user.Email,
+			Hash:  string(hash),
+		}
+
+		// сериализуем в JSON
+		file, err := os.Create("user.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		encoder := json.NewEncoder(file)
+		encoder.SetIndent("", "  ")
+		if err := encoder.Encode(jsonUser); err != nil {
+			log.Fatal(err)
+		}
+
+		text := fmt.Sprintf("http://localhost:8081/verify/%s", string(hash))
 		e.Text = []byte(text)
 		e.HTML = []byte("<h1>Fancy HTML is supported, too!</h1>")
-		e.Send("smtp.gmail.com:587", smtp.PlainAuth("", handler.Email, handler.Pass, "smtp.gmail.com"))
+		e.Send("smtp.gmail.com:587", smtp.PlainAuth("", user.Email, user.Password, "smtp.gmail.com"))
 		json.NewEncoder(w).Encode(text)
 	}
 }
@@ -42,11 +84,20 @@ func (handler *EmailIns) Send() http.HandlerFunc {
 func (handler *EmailIns) Verify() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		hash := req.PathValue("hash")
-		if hash == hashStr {
-			json.NewEncoder(w).Encode(true)
-		} else {
-			json.NewEncoder(w).Encode(false)
+		data, err := os.ReadFile("user.json")
+		if err != nil {
+			log.Fatal(err)
 		}
 
+		var loaded JsonUser
+		if err := json.Unmarshal(data, &loaded); err != nil {
+			log.Fatal(err)
+		}
+		if loaded.Hash == hash {
+			json.NewEncoder(w).Encode(true)
+		} else {
+			os.Remove("user.json")
+			json.NewEncoder(w).Encode(false)
+		}
 	}
 }
